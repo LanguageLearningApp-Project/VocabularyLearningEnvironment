cbd656c18919b1d3528b2db5da36ecd72ca32d65
Zeynep Tasevli <zeynep.tasevli@ug.bilkent.edu.tr>
2025-08-14 17:20:22 +0200

    Vocabulary model is constructed
    For the database

--- VocabularyLearningEnvironment/vocab/models.py
+++ VocabularyLearningEnvironment/vocab/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Vocabulary(models.Model):
+    question_word = models.CharField(max_length=100)
+    answer_word = models.CharField(max_length=100)
+
+    question_language = models.CharField(max_length=50)
+    answer_language = models.CharField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return f"{self.question_word} ({self.question_language}) -> {self.answer_word} ({self.answer_language})"
+    
+class Meta:
+    verbose_name = "Vocabulary Word"
+    verbose_name_plural = "Vocabulary Words"
