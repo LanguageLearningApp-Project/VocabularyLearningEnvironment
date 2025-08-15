32a3ffcbf59916f7f08e6b2508e77a1181e6d6e2
elifDuman <eduman@ug.bilkent.edu.tr>
2025-08-15 10:48:56 +0200

    Auto stash before merge of "main" and "origin/main"

--- VocabularyLearningEnvironment/vocab/views.py
+++ VocabularyLearningEnvironment/vocab/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from components.teacher.planners import RandomPlanner 
+from wordfreq import top_n_list
+
+word_list = top_n_list("en", 5000)[200:]
 
 def home(request):
     return render(request, "vocab/home.html")
 
 def random_word_view(request):
     planner = RandomPlanner()
-    word = planner.choose_item()
+    word = planner.choose_item(word_list, context=None, time=0)
     return JsonResponse({"word": word})
